bfa06e69bc47bcb8ca22c78af1753c4405b1d55e
Bridger Tower <bridgertower@gmail.com>
2025-08-04 16:09:34 -0600

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main content="text-sm font-mono uppercase">
+    <main className="flex flex-col gap-2 text-sm uppercase p-6">
       <h1>brijr is a web developer</h1>
+      <Spacer />
+      <h2>
+        &gt; website: <a href="https://bridger.to">bridger.to</a>
+      </h2>
       <p>
-        name: <a href="https://bridger.to">bridger tower</a>
+        &gt; github: <a href="https://github.com/brijr">@brijr</a>
       </p>
-      <p>"i like to make websites"</p>
+      <p>
+        &gt; x: <a href="https://x.com/bridgertower">@bridgertower</a>
+      </p>
+      <Spacer />
+      <p className="orange">"i like to make websites"</p>
+      <p>© {new Date().getFullYear()} </p>
     </main>
   );
 }
+
+const Spacer = () => <div className="h-4" />;
